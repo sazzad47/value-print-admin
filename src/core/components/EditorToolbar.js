83575532e45571feb4b3a445e96d7be7a045934a
Sazzad <sazzad47.ju@gmail.com>
2023-09-17 19:15:50 +0600

    make it better

--- src/core/components/EditorToolbar.js
+++ src/core/components/EditorToolbar.js
@@ -1,31 +1,31 @@
 import React from "react";
 import { Quill } from "react-quill";
 import { imageUpload } from "../utils/imageUpload";
-import { videoUpload } from "../utils/videoUpload";
+// import { videoUpload } from "../utils/videoUpload";
 
 // Custom Undo button icon component for Quill editor. You can import it directly
 // from 'quill/assets/icons/undo.svg' but I found that a number of loaders do not
 // handle them correctly
-const CustomUndo = () => (
-  <svg viewBox="0 0 18 18">
-    <polygon className="ql-fill ql-stroke" points="6 10 4 12 2 10 6 10" />
-    <path
-      className="ql-stroke"
-      d="M8.09,13.91A4.6,4.6,0,0,0,9,14,5,5,0,1,0,4,9"
-    />
-  </svg>
-);
+// const CustomUndo = () => (
+//   <svg viewBox="0 0 18 18">
+//     <polygon className="ql-fill ql-stroke" points="6 10 4 12 2 10 6 10" />
+//     <path
+//       className="ql-stroke"
+//       d="M8.09,13.91A4.6,4.6,0,0,0,9,14,5,5,0,1,0,4,9"
+//     />
+//   </svg>
+// );
 
 // Redo button icon component for Quill editor
-const CustomRedo = () => (
-  <svg viewBox="0 0 18 18">
-    <polygon className="ql-fill ql-stroke" points="12 10 14 12 16 10 12 10" />
-    <path
-      className="ql-stroke"
-      d="M9.91,13.91A4.6,4.6,0,0,1,9,14a5,5,0,1,1,5-5"
-    />
-  </svg>
-);
+// const CustomRedo = () => (
+//   <svg viewBox="0 0 18 18">
+//     <polygon className="ql-fill ql-stroke" points="12 10 14 12 16 10 12 10" />
+//     <path
+//       className="ql-stroke"
+//       d="M9.91,13.91A4.6,4.6,0,0,1,9,14a5,5,0,1,1,5-5"
+//     />
+//   </svg>
+// );
 
 // Add sizes to whitelist and register them
 const Size = Quill.import("formats/size");
@@ -46,12 +46,12 @@ Font.whitelist = [
 Quill.register(Font, true);
 
 // Undo and redo functions for Custom Toolbar
-function undoChange() {
-  this.quill.history.undo();
-}
-function redoChange() {
-  this.quill.history.redo();
-}
+// function undoChange() {
+//   this.quill.history.undo();
+// }
+// function redoChange() {
+//   this.quill.history.redo();
+// }
 
 // Image handler
 function imageHandler() {
@@ -89,39 +89,39 @@ function imageHandler() {
 }
 
 // video handler
-function videoHandler() {
-  const input = document.createElement("input");
-  input.setAttribute("type", "file");
-  input.setAttribute("accept", "video/mp4, video/x-m4v, video/*");
-  input.click();
+// function videoHandler() {
+//   const input = document.createElement("input");
+//   input.setAttribute("type", "file");
+//   input.setAttribute("accept", "video/mp4, video/x-m4v, video/*");
+//   input.click();
 
-  input.onchange = async () => {
-    const file = input.files ? input.files[0] : null;
+//   input.onchange = async () => {
+//     const file = input.files ? input.files[0] : null;
 
-    const quillObj = this.quill;
-    const range = quillObj?.getSelection();
-    if (file) {
-      const loader = document.getElementById("quill_file_loading");
-      try {
-        loader.style.display = "flex";
-        // upload the video file to the server using an async function
-        const media = await videoUpload(file);
+//     const quillObj = this.quill;
+//     const range = quillObj?.getSelection();
+//     if (file) {
+//       const loader = document.getElementById("quill_file_loading");
+//       try {
+//         loader.style.display = "flex";
+//         // upload the video file to the server using an async function
+//         const media = await videoUpload(file);
 
-        // log the server response for debugging purposes
-        console.log("file", media);
+//         // log the server response for debugging purposes
+//         console.log("file", media);
 
-        // insert the uploaded video into the editor at the current cursor position
-        quillObj.editor.insertEmbed(range.index, "video", media);
-        loader.style.display = "none";
-      } catch (error) {
-        loader.style.display = "none";
-        // handle errors with the file upload or insertion
-        console.error("Error uploading video:", error);
-        alert("There was an error uploading the video. Please try again.");
-      }
-    }
-  };
-}
+//         // insert the uploaded video into the editor at the current cursor position
+//         quillObj.editor.insertEmbed(range.index, "video", media);
+//         loader.style.display = "none";
+//       } catch (error) {
+//         loader.style.display = "none";
+//         // handle errors with the file upload or insertion
+//         console.error("Error uploading video:", error);
+//         alert("There was an error uploading the video. Please try again.");
+//       }
+//     }
+//   };
+// }
 
 // Modules object for setting up the Quill editor
 export const modules = (props) => {
@@ -129,10 +129,10 @@ export const modules = (props) => {
     toolbar: {
       container: "#" + props,
       handlers: {
-        undo: undoChange,
-        redo: redoChange,
+        // undo: undoChange,
+        // redo: redoChange,
         image: imageHandler,
-        video: videoHandler,
+        // video: videoHandler,
       },
     },
     history: {
@@ -146,24 +146,24 @@ export const modules = (props) => {
 // Formats objects for setting up the Quill editor
 export const formats = [
   "header",
-  "font",
+  // "font",
   "size",
   "bold",
   "italic",
   "underline",
-  "align",
-  "strike",
-  "script",
-  "blockquote",
+  // "align",
+  // "strike",
+  // "script",
+  // "blockquote",
   "background",
   "list",
   "bullet",
-  "indent",
+  // "indent",
   "link",
   "image",
-  "video",
-  "color",
-  "code-block",
+  // "video",
+  // "color",
+  // "code-block",
 ];
 
 // Quill Toolbar component
@@ -176,10 +176,10 @@ export const QuillToolbar = (props) => {
             <button className="ql-bold" />
             <button className="ql-italic" />
             <button className="ql-underline" />
-            <button className="ql-strike" />
+            {/* <button className="ql-strike" /> */}
           </span>
           <span className="ql-formats">
-            <select className="ql-font">
+            {/* <select className="ql-font">
               <option value="arial"> Arial </option>
               <option value="comic-sans">Comic Sans</option>
               <option value="courier-new">Courier New</option>
@@ -189,7 +189,7 @@ export const QuillToolbar = (props) => {
                 Inter
               </option>
               <option value="lucida">Lucida</option>
-            </select>
+            </select> */}
             <select className="ql-size">
               <option value="extra-small">Extra Small</option>
               <option value="small">Small</option>
@@ -213,38 +213,38 @@ export const QuillToolbar = (props) => {
           <span className="ql-formats">
             <button className="ql-list" value="ordered" />
             <button className="ql-list" value="bullet" />
-            <button className="ql-indent" value="-1" />
-            <button className="ql-indent" value="+1" />
+            {/* <button className="ql-indent" value="-1" />
+            <button className="ql-indent" value="+1" /> */}
           </span>
-          <span className="ql-formats">
+          {/* <span className="ql-formats">
             <button className="ql-script" value="super" />
             <button className="ql-script" value="sub" />
             <button className="ql-blockquote" />
             <button className="ql-direction" />
-          </span>
+          </span> */}
           <span className="ql-formats">
-            <select className="ql-align" />
-            <select className="ql-color" />
-            <select className="ql-background" />
+            {/* <select className="ql-align" />
+            <select className="ql-color" /> */}
+            {/* <select className="ql-background" /> */}
           </span>
           <span className="ql-formats">
             <button className="ql-link" />
             <button className="ql-image" />
-            <button className="ql-video" />
+            {/* <button className="ql-video" /> */}
           </span>
-          <span className="ql-formats">
+          {/* <span className="ql-formats">
             <button className="ql-formula" />
             <button className="ql-code-block" />
             <button className="ql-clean" />
-          </span>
-          <span className="ql-formats">
+          </span> */}
+          {/* <span className="ql-formats">
             <button className="ql-undo">
               <CustomUndo />
             </button>
             <button className="ql-redo">
               <CustomRedo />
             </button>
-          </span>
+          </span> */}
         </div>
       )}
     </>
--- src/core/components/TextEditor.js
+++ src/core/components/TextEditor.js
@@ -1,212 +1,48 @@
-import React, { useRef, useState, useEffect } from "react";
-import MUIRichTextEditor from "mui-rte";
-import Grid from "@mui/material/Grid";
-import { makeStyles } from "@mui/styles";
-import Popover from "@mui/material/Popover";
-import TextField from "@mui/material/TextField";
-import IconButton from "@mui/material/IconButton";
-import Button from "@mui/material/Button";
-import BackupIcon from "@mui/icons-material/Backup";
-import DoneIcon from "@mui/icons-material/Done";
-import CloseIcon from "@mui/icons-material/Close";
-import AttachFileIcon from "@mui/icons-material/AttachFile";
-import { imageUpload } from "../utils/imageUpload";
-import { stateToHTML } from "draft-js-export-html";
+import React, { useRef } from "react";
+import EditorToolbar, { modules, formats } from "./EditorToolbar";
+import "react-quill/dist/quill.snow.css";
+import { Blocks } from "react-loader-spinner";
 
-const cardPopverStyles = makeStyles({
-  root: {
-    padding: 10,
-    maxWidth: 350,
-  },
-  textField: {
-    width: "100%",
-  },
-  input: {
-    display: "none",
-  },
-});
+import ReactQuill from "react-quill";
 
-const uploadImage = (file) => {
-  return new Promise(async (resolve, reject) => {
-    const url = await imageUpload([file]);
-    if (!url) {
-      reject();
-      return;
-    }
-    resolve({
-      data: {
-        url: url[0],
-        width: 300,
-        height: 200,
-        alignment: "left", // or "center", "right"
-        type: "image", // or "video"
-      },
-    });
-  });
-};
+const TextEditor = ({ value, state, setState }) => {
+  const quillRef = useRef(null);
 
-const UploadImagePopover = (props) => {
-  const classes = cardPopverStyles(props);
-  const [state, setState] = useState({
-    anchor: null,
-    isCancelled: false,
-  });
-  const [data, setData] = useState({});
-
-  useEffect(() => {
-    setState({
-      anchor: props.anchor,
-      isCancelled: false,
-    });
-    setData({
-      file: undefined,
-    });
-  }, [props.anchor]);
-
-  return (
-    <Popover
-      anchorEl={state.anchor}
-      open={state.anchor !== null}
-      anchorOrigin={{
-        vertical: "bottom",
-        horizontal: "right",
-      }}
-      transformOrigin={{
-        vertical: "top",
-        horizontal: "left",
-      }}
-    >
-      <Grid container spacing={1} className={classes.root}>
-        <Grid item xs={10}>
-          <TextField
-            className={classes.textField}
-            disabled
-            value={data.file?.name || ""}
-            placeholder="Click icon to attach image"
-          />
-        </Grid>
-        <Grid item xs={2}>
-          <input
-            accept="image/*"
-            className={classes.input}
-            id="contained-button-file"
-            type="file"
-            onChange={(event) => {
-              setData({
-                ...data,
-                file: event.target.files[0],
-              });
-            }}
-          />
-          <label htmlFor="contained-button-file">
-            <IconButton
-              color="primary"
-              aria-label="upload image"
-              component="span"
-            >
-              <AttachFileIcon />
-            </IconButton>
-          </label>
-        </Grid>
-        <Grid item container xs={12} justifyContent="flex-end">
-          <Button
-            onClick={() => {
-              setState({
-                anchor: null,
-                isCancelled: true,
-              });
-            }}
-          >
-            <CloseIcon />
-          </Button>
-          <Button
-            onClick={() => {
-              setState({
-                anchor: null,
-                isCancelled: false,
-              });
-              props.onSubmit(data, !state.isCancelled);
-            }}
-          >
-            <DoneIcon />
-          </Button>
-        </Grid>
-      </Grid>
-    </Popover>
-  );
-};
-
-const AsyncImageUpload = ({ value, state, setState }) => {
-  const ref = useRef(null);
-  const [anchor, setAnchor] = useState(null);
-
-  const handleFileUpload = (file) => {
-    ref.current?.insertAtomicBlockAsync(
-      "IMAGE",
-      uploadImage(file),
-      "Uploading now..."
-    );
-  };
-
-  const handleChange = (data) => {
-    const expected = stateToHTML(data.getCurrentContent());
-    setState((prevData) => ({ ...prevData, [state]: expected }));
+  const editorStyles = {
+    backgroundColor: "lightblue", color: "black", 
   };
 
   return (
     <div>
-      <UploadImagePopover
-        anchor={anchor}
-        onSubmit={(data, insert) => {
-          if (insert && data.file) {
-            handleFileUpload(data.file);
-          }
-          setAnchor(null);
-        }}
-      />
-      <MUIRichTextEditor
-        onChange={(data) => handleChange(data)}
-        label="Write someting awesome..."
-        ref={ref}
-        controls={[
-          "title",
-          "bold",
-          "italic",
-          "underline",
-          "strikethrough",
-          "highlight",
-          "link",
-          "media",
-          "upload-image",
-          "numberList",
-          "bulletList",
-          "quote",
-          "code",
-          "undo",
-          "redo",
-        ]}
-        customControls={[
-          {
-            name: "upload-image",
-            icon: <BackupIcon />,
-            type: "callback",
-            onClick: (_editorState, _name, anchor) => {
-              setAnchor(anchor);
-            },
-          },
-        ]}
-        draftEditorProps={{
-          handleDroppedFiles: (_selectionState, files) => {
-            if (files.length && files[0].name !== undefined) {
-              handleFileUpload(files[0]);
-              return "handled";
-            }
-            return "not-handled";
-          },
-        }}
+      <div
+        id="quill_file_loading"
+        style={{ display: "none" }}
+        className="fixed top-0 right-0 bottom-0 left-0 items-center justify-center "
+      >
+        <Blocks
+          visible={true}
+          height={80}
+          width={80}
+          ariaLabel="blocks-loading"
+          wrapperStyle={{}}
+          wrapperClass="blocks-wrapper"
+        />
+      </div>
+      <EditorToolbar toolbarId={"t1"} />
+      <ReactQuill
+        ref={quillRef}
+        theme="snow"
+        value={value}
+        onChange={(value) =>
+          setState((prevData) => ({ ...prevData, [state]: value }))
+        }
+        placeholder={"Write something awesome..."}
+        modules={modules("t1")}
+        formats={formats}
+        style={editorStyles}
       />
     </div>
   );
 };
 
-export default AsyncImageUpload;
+export default TextEditor;
